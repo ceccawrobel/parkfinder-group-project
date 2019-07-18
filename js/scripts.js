4dca0db762d3cf1a685b55633e379332f3eb4354
Winter Runion <winterrunion@gmail.com>
2019-07-17 17:28:46 -0700

    Add JS arrays and functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,6 @@
-/*How to get values from textboxes, dates, and drop downs:*/
-var beverage = $("#beverage").val();
-
-/*How to get values from radio buttons:*/
-var flavor = $("input:radio[name=flavor]:checked").val();
-
-
-/*How to get values from checkboxes:*/
-var flavor = $("input:checkbox[name=flavor]:checked").val();
-
-//
-/*Sample script we may want to copy from*/
-// <script type="text/javascript">
-// function doSearch(roomquery) {
-// alert('roomquery is ' + roomquery);
-// }
-// </script>
-//
-// how many rooms? <select onchange="doSearch(this.options[selectedIndex].text);">
-//     <option>1</option>
-//     <option>2</option>
-// </select>
-//Source: http: jsfiddle.net/H9jkd/
-
 //Business Logic
-//Creating the "Park" object constructor function. Reference: https://www.learnhowtoprogram.com/intro-to-programming-evening/object-oriented-javascript-3cb65911-fc7b-4e3d-ae84-8af3cbed4b34/constructors-and-prototypes
-function Park(playground, parking, garden, restroom, accessibility, zone, pool) {
+function Park(name, playground, parking, garden, restroom, accessibility, zone, pool) {
+  this.name = name;
   this.playground = playground;
   this.parking = parking;
   this.garden = garden;
@@ -34,114 +10,191 @@ function Park(playground, parking, garden, restroom, accessibility, zone, pool) 
   this.pool = pool;
 }
 
-var rainbowPark = new Park(true, false, false, true, false, false, true);
-var unicornPark = new Park(true, true, true, true, true, false, true);
-var dragonPark = new Park(false, true, true, true, true, true, true);
-var hobbitPark = new Park(true, true, true, true, true, false, false);
-var galaxyPark = new Park(false, true, false, false, true, true, false);
+//Create a new object for each park
+var rainbowPark = new Park("Rainbow Park", true, false, true, true, false, true, true);
+var unicornPark = new Park("Unicorn Park", true, true, false, true, true, false, true);
+var dragonPark = new Park("Dragon Park", false, true, true, true, false, true, true);
+var hobbitPark = new Park("Hobbit Park", true, false, true, true, true, false, false);
+var galaxyPark = new Park("Galaxy Park", false, true, true, true, false, true, false);
 
-//Functions for amenities checkboxes
+//Array of park objects
+var parkObjects = [rainbowPark, unicornPark, dragonPark, hobbitPark, galaxyPark];
 
+var playgroundParks = [];
+var parkingParks = [];
+var gardenParks = [];
+var restroomParks = [];
+var accessibilityParks = [];
+var zoneParks = [];
+var poolParks = [];
 
+function findPlaygroundParks(park) {
+  parkObjects.forEach(function(park) {
+    if (park.playground == true) {
+      playgroundParks.push(park.name);
+    }
+  });
+}
 
-// function rainbowPark() {
-//   // var text = document.getElementById("playground");
-//   var text = document.getElementById("rainbow-result");
-//   text.style.display = "block";
-//   // if (checkBox.checked == true) {
-//   //   text.style.display = "block";
-//   // } else {
-//   //   text.style.display = "none";
-//   // }
-// }
+function findParkingParks(park) {
+  parkObjects.forEach(function(park) {
+    if (park.parking == true) {
+      parkingParks.push(park.name);
+    }
+  });
+}
 
-// function rainbowPark() {
-//   // Get the checkbox
-//   var checkBox = document.getElementById("playground");
-//   // Get the output text
-//   var text = document.getElementById("rainbow-result");
-//   // If the checkbox is checked, display the output text
-//   if (checkBox.checked == true) {
-//     text.style.display = "block";
-//   } else {
-//     text.style.display = "none";
-//   }
-// }
+function findGardenParks(park) {
+  parkObjects.forEach(function(park) {
+    if (park.garden == true) {
+      gardenParks.push(park.name);
+    }
+  });
+}
 
+function findRestroomParks(park) {
+  parkObjects.forEach(function(park) {
+    if (park.restroom == true) {
+      restroomParks.push(park.name);
+    }
+  });
+}
+
+function findAccesibilityParks(park) {
+  parkObjects.forEach(function(park) {
+    if (park.accessibility == true) {
+      accessibilityParks.push(park.name);
+    }
+  });
+}
+
+function findZoneParks(park) {
+  parkObjects.forEach(function(park) {
+    if (park.zone == true) {
+      zoneParks.push(park.name);
+    }
+  });
+}
+
+function findPoolParks(park) {
+  parkObjects.forEach(function(park) {
+    if (park.pool == true) {
+      poolParks.push(park.name);
+    }
+  });
+}
+
+//Array of park name KEYWORDS for manipulating the DOM
+var parkNames = ["rainbow", "unicorn", "dragon", "hobbit", "galaxy"];
+
+//To show all parks with jQuery
+function showAllParks() {
+  parkNames.forEach(function(park) {
+    $("#" + park + "-result").show();
+  });
+}
+
+//To hide all parks with jQuery
+function hideAllParks() {
+  parkNames.forEach(function(park) {
+    $("#" + park + "-result").hide();
+  });
+}
 
 // function showPlaygroundParks() {
-//   rainbowPark();
+//
+// }
+//
+// function showParkingParks() {
+//
+// }
+//
+// function showGardenParks() {
+//
 // }
 
+//OLD CODE BELOW. NEEDS TO BE REFACTORED. (Refactoring taking place above)
 
-//Functions for park checkboxes
+//Simple functions to show the parks (jQuery version)
 function showRainbowPark() {
-  // Get the checkbox
-  var checkBox = document.getElementById("rainbow");
-  // Get the output text
-  var text = document.getElementById("rainbow-result");
-  // If the checkbox is checked, display the output text
-  if (checkBox.checked == true) {
-    text.style.display = "block";
-  } else {
-    text.style.display = "none";
-  }
+  $("#rainbow-result").show();
 }
 
 function showUnicornPark() {
-  // Get the checkbox
-  var checkBox = document.getElementById("unicorn");
-  // Get the output text
+  $("#unicorn-result").show();
+}
+
+
+//Simple functions to show the parks (pure JS version)
+function showUnicornPark() {
   var text = document.getElementById("unicorn-result");
-  // If the checkbox is checked, display the output text
-  if (checkBox.checked == true) {
-    text.style.display = "block";
-  } else {
-    text.style.display = "none";
-  }
+  text.style.display = "block";
 }
 
 function showDragonPark() {
-  // Get the checkbox
-  var checkBox = document.getElementById("dragon");
-  // Get the output text
   var text = document.getElementById("dragon-result");
-  // If the checkbox is checked, display the output text
-  if (checkBox.checked == true) {
-    text.style.display = "block";
-  } else {
-    text.style.display = "none";
-  }
+  text.style.display = "block";
 }
 
 function showHobbitPark() {
-  // Get the checkbox
-  var checkBox = document.getElementById("hobbit");
-  // Get the output text
   var text = document.getElementById("hobbit-result");
-  // If the checkbox is checked, display the output text
-  if (checkBox.checked == true) {
-    text.style.display = "block";
-  } else {
-    text.style.display = "none";
-  }
+  text.style.display = "block";
 }
 
 function showGalaxyPark() {
-  // Get the checkbox
-  var checkBox = document.getElementById("galaxy");
-  // Get the output text
   var text = document.getElementById("galaxy-result");
-  // If the checkbox is checked, display the output text
-  if (checkBox.checked == true) {
-    text.style.display = "block";
+  text.style.display = "block";
+}
+
+//Simple functions to hide the parks
+function hideRainbowPark() {
+  $("#rainbow-result").hide();
+}
+
+function hideUnicornPark() {
+  var text = document.getElementById("unicorn-result");
+  text.style.display = "none";
+}
+
+function hideDragonPark() {
+  var text = document.getElementById("dragon-result");
+  text.style.display = "none";
+}
+
+function hideHobbitPark() {
+  var text = document.getElementById("hobbit-result");
+  text.style.display = "none";
+}
+
+function hideGalaxyPark() {
+  var text = document.getElementById("galaxy-result");
+  text.style.display = "none";
+}
+
+//Complex functions sorted by feature
+function showPlayground() {
+  var checkBox = document.getElementById("playground");
+  var text = document.getElementById("playground-result");
+  if (checkBox.checked == true){
+  text.style.display = "block";
   } else {
-    text.style.display = "none";
-  }
+   text.style.display = "none";
+ }
 }
 
 //UI Logic
+$(document).ready(function() {
 
-// Resources:
-//
-// check out https//developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+  //Hide results div
+  $(".hidden").hide();
+
+  //Run all the functions to populate the arrays with the corresponding park names. This doesn't put the text anywhere; it just populates the arrays, which we then can display when we want to.
+  findPlaygroundParks();
+  findParkingParks();
+  findGardenParks();
+  findRestroomParks();
+  findAccesibilityParks();
+  findZoneParks();
+  findPoolParks();
+
+});
